508a3f3b2b14fc25b8fcbbc2ec835a2414c6a29d
Jackeagle <jackeagle102@gmail.com>
2020-05-03 19:17:48 +0200

    cheryl: Set Bootanimation Res
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_cheryl.mk
+++ bliss_cheryl.mk
@@ -17,6 +17,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
